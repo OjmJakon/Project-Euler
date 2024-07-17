edadba9573e93acef656ba27974c3e587f21ec1d
Ziga Petelin <64520596+OjmJakon@users.noreply.github.com>
2024-07-17 17:33:18 +0200

    Added first attempt code

--- Python/Problem 10 - Summation of primes.py
+++ Python/Problem 10 - Summation of primes.py
@@ -1,6 +1,16 @@
-result = []
-for i in range(2,10):
-    for x in range(2,i):
-        if i % x != 0
+#Problem:
+#The sum of primes below 10 is 2+3+5+7=17
+#Find the sum of all primes below two million
 
-print(result)
+#Code:
+#arr=[]
+#for i in range(2,2000000):
+        #for j in range (2,i):
+            #if i % j == 0:
+                #break
+        #else:
+            #arr.append(i)
+#print(sum(arr))
+
+#Takes too long. Upon further research it is better to use the Sieve algorithm.
+
