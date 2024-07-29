6fc87ff4f98de0de0c63cc94a93afa78ded2aaa1
Ziga Petelin <64520596+OjmJakon@users.noreply.github.com>
2024-07-29 17:06:29 +0200

    Create 005 - Smallest Multiple.py
    Created plan for problem 005

---
+++ Python/005 - Smallest Multiple.py
@@ -0,0 +1,4 @@
+#Problem: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smalest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+#Code:
+#The plan is to create a for loop that iterates through numbers and tests if each number is divisible from 1 to 20 without leaving any remainder. When it finds this number, it exits the loop.
